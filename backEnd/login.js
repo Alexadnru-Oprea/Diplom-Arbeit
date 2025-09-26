5071cbc08efa74203db5a0663887b7cb0d1862b3
GitLisa <elizaveta.konrath@htlpinkafeld.at>
2025-09-26 20:16:31 +0200

    Login und Reg untereinander

--- backEnd/login.js
+++ backEnd/login.js
@@ -1,29 +1,28 @@
-document.getElementById("registerBtn").addEventListener("click", async () => {
-  const username = document.getElementById("reg-username").value;
-  const password = document.getElementById("reg-password").value;
-  const res = await fetch("http://localhost:3000/register", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username, password })
-});
-  const data = await res.json();
-  alert(data.message);
-});
-
 document.getElementById("loginBtn").addEventListener("click", async () => {
   const username = document.getElementById("login-username").value;
   const password = document.getElementById("login-password").value;
   const res = await fetch("http://localhost:3000/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username, password })
-});
-      
-const data = await res.json();
-alert(data.message);
+    body: JSON.stringify({ username, password }),
+  });
+
+  document.getElementById("registerBtn").addEventListener("click", async () => {
+    const username = document.getElementById("reg-username").value;
+    const password = document.getElementById("reg-password").value;
+    const res = await fetch("http://localhost:3000/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await res.json();
+    alert(data.message);
+  });
+
+  const data = await res.json();
+  alert(data.message);
   if (data.message === "Login erfolgreich!") {
     localStorage.setItem("username", username);
     window.location.href = "gruppen.html";
   }
 });
-
